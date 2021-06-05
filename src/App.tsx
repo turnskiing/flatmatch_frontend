06c7ed788e794e95b8717198b1072b5839753f67
Manuel Klettner <manuel.klettner@tum.de>
2021-06-06 00:17:41 +0200

    Add initial router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import SignInView  from './views/SignInView'
+import Router  from './Router'
 
 function App() {
-  return (
-    <div>
-      <head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </head>
-      <SignInView/>
-    </div>
-  );
+  return <Router />
 }
 
 export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import SignInView from './views/SignInView';
+import FindOfferingView from './views/FindOffering/FindOfferingView';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path="/" component={SignInView} />
+        <Route path="/sign_in" component={SignInView} />
+        <Route path="/home/find_room" component={FindOfferingView} />
+        {/* Not Found */}
+        <Route component={() => <Redirect to="/" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
