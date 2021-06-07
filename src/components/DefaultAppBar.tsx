a6384fcdeff6af6cb47a6c8c32a7a7f8a5efc5fc
Manuel Klettner <manuel.klettner@tum.de>
2021-06-07 21:49:18 +0200

    Add avatar and logout menu

--- src/components/DefaultAppBar.tsx
+++ src/components/DefaultAppBar.tsx
@@ -4,13 +4,26 @@ import Typography from "@material-ui/core/Typography";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 import Toolbar from "@material-ui/core/Toolbar";
-import AccountCircle from "@material-ui/icons/AccountCircle";
+import Avatar from "@material-ui/core/Avatar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { Grid, IconButton } from "@material-ui/core";
+
 import Logo from "../images/FlatMatch.png";
-import { Grid } from "@material-ui/core";
+import React from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  appbar: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+  },
+  avatar: {
+    backgroundColor: "#ff8f00",
   },
   logo: {
     width: 150,
@@ -20,12 +33,48 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DefaultAppBar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "appbar-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <Link color="inherit" href="/signIn">
+          Logout
+        </Link>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appbar}>
         <Toolbar>
-          <Grid justify="space-between" alignItems="center" container spacing={0}>
+          <Grid
+            justify="space-between"
+            alignItems="center"
+            container
+            spacing={0}
+          >
             <Grid item>
               <Breadcrumbs maxItems={1} aria-label="breadcrumb">
                 <Link color="inherit" href="/">
@@ -38,11 +87,27 @@ export default function DefaultAppBar() {
               <img src={Logo} className={classes.logo} alt="" />
             </Grid>
             <Grid item>
-              <AccountCircle />
+              <IconButton
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <Avatar
+                  src="/broken-image.jpg"
+                  variant="rounded"
+                  className={classes.avatar}
+                >
+                  H
+                </Avatar>
+              </IconButton>
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
+      {renderMenu}
     </div>
   );
 }
