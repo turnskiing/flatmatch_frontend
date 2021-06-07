89d3b446893dfabd79fb556cbf2b7bb63e89f19f
Manuel Klettner <manuel.klettner@tum.de>
2021-06-07 10:53:48 +0200

    Add formater + linter and improve tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core"
+import App from "./App"
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#ff8f00", // This is an orange looking color
-    },
-    secondary: {
-      main: "#ffcc80", //Another orange-ish color
-    },
-  },
-});
+	palette: {
+		primary: {
+			main: "#ff8f00", // This is an orange looking color
+		},
+		secondary: {
+			main: "#ffcc80", // Another orange-ish color
+		},
+	},
+})
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>,
-  document.getElementById("root")
-);
+	<ThemeProvider theme={theme}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</ThemeProvider>,
+	document.getElementById("root")
+)
