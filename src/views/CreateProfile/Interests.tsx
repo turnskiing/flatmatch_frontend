2b3736a055f495fe09547a296619fad99609d73e
Manuel Klettner <manuel.klettner@tum.de>
2021-06-23 10:50:59 +0200

    Add interests chips and custom text field

--- src/views/CreateProfile/Interests.tsx
+++ src/views/CreateProfile/Interests.tsx
@@ -1,95 +1,178 @@
-import React, { useContext } from "react";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+import React, { useContext } from "react"
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
+import TextField from "@material-ui/core/TextField"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
+import Chip from "@material-ui/core/Chip"
+import Paper from "@material-ui/core/Paper"
+import IconButton from "@material-ui/core/IconButton"
+import Input from "@material-ui/core/Input"
+import InputLabel from "@material-ui/core/InputLabel"
+import InputAdornment from "@material-ui/core/InputAdornment"
+import FormControl from "@material-ui/core/FormControl"
+import AddIcon from "@material-ui/icons/Add"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 // Context
-import { UserContext } from "../../App";
+import { UserContext } from "../../App"
 // Models
-import { IUser } from "../../models/user";
+import { IUser } from "../../models/user"
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			display: "flex",
+			justifyContent: "center",
+			flexWrap: "wrap",
+			listStyle: "none",
+			padding: theme.spacing(0.5),
+			margin: 0,
+		},
+		chip: {
+			margin: theme.spacing(0.5),
+		},
+		margin: {
+			margin: theme.spacing(1),
+		},
+		withoutLabel: {
+			marginTop: theme.spacing(3),
+		},
+	})
+)
 
 export default function Interests() {
-  const userContext = useContext(UserContext);
+	const classes = useStyles()
+	const userContext = useContext(UserContext)
+	const [interest, setInterest] = React.useState("")
 
-  const setBio = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser: IUser = {
-      ...userContext.user,
-      bio: event.target.value,
-    };
-    userContext.setUser(newUser);
-  };
+	const setBio = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newUser: IUser = {
+			...userContext.user,
+			bio: event.target.value,
+		}
+		userContext.setUser(newUser)
+	}
 
-  const setInterests = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser: IUser = {
-      ...userContext.user,
-      interests: [event.target.value],
-    };
-    userContext.setUser(newUser);
-  };
+	const setInterests = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newUser: IUser = {
+			...userContext.user,
+			interests: [event.target.value],
+		}
+		userContext.setUser(newUser)
+	}
 
-  const setAcceptedTerms = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser: IUser = {
-      ...userContext.user,
-      acceptedTerms: event.target.checked,
-    };
-    userContext.setUser(newUser);
-  };
+	const setAcceptedTerms = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newUser: IUser = {
+			...userContext.user,
+			acceptedTerms: event.target.checked,
+		}
+		userContext.setUser(newUser)
+	}
 
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Interests
-      </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Typography variant="subtitle1" gutterBottom>
-            Bio
-          </Typography>
-          <TextField
-            id="bio"
-            label="Describe yourself briefly (max 200 characters)"
-            multiline
-            fullWidth
-            rows={4}
-            inputProps={{
-              maxLength: 200,
-            }}
-            defaultValue=""
-            variant="outlined"
-            value={userContext.user.bio}
-            onChange={setBio}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Typography variant="subtitle1" gutterBottom>
-            What are you interested in?
-          </Typography>
-          <TextField
-            required
-            id="Add-interests-icon-here"
-            label="Add interests icon here"
-            helperText="Add interests icon here"
-            fullWidth
-            autoComplete="cc-csc"
-            value={userContext.user.interests}
-            onChange={setInterests}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                color="primary"
-                name="termsOfService"
-                checked={userContext.user.acceptedTerms}
-                onChange={setAcceptedTerms}
-              />
-            }
-            label="I accept the Terms of Service"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+	const [chipData, setChipData] = React.useState<string[]>([])
+
+	const handleDelete = (chipToDelete: string) => () => {
+		setChipData((chips) =>
+			chips.filter((chip) => chip !== chipToDelete)
+		)
+	}
+
+	const handleChange = () => (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInterest(event.target.value)
+	}
+
+	const addInterest = () => {
+		if (interest.trim() !== "" && chipData.length < 6 && chipData.filter(b => b === interest.trim()).length === 0) {
+			const newchipData = chipData
+
+			newchipData.push(interest.trim())
+			setInterest("")
+			setChipData(newchipData)
+		}
+	}
+
+	return (
+		<React.Fragment>
+			<Typography variant="h6" gutterBottom>
+				Interests
+			</Typography>
+			<Grid container spacing={3}>
+				<Grid item xs={12}>
+					<Typography variant="subtitle1" gutterBottom>
+						Bio
+					</Typography>
+					<TextField
+						id="bio"
+						label="Describe yourself briefly (max 200 characters)"
+						multiline
+						fullWidth
+						rows={4}
+						inputProps={{
+							maxLength: 200,
+						}}
+						defaultValue=""
+						variant="outlined"
+						value={userContext.user.bio}
+						onChange={setBio}
+					/>
+				</Grid>
+				<Grid item xs={12} sm={5}>
+					<Typography variant="subtitle1" gutterBottom>
+						What are you interested in?
+					</Typography>
+					<FormControl fullWidth className={classes.margin}>
+						<InputLabel htmlFor="standard-adornment-interests">
+							Interest
+						</InputLabel>
+						<Input
+							id="interests-textfield"
+							value={interest}
+							onChange={handleChange()}
+							endAdornment={
+								<InputAdornment position="end">
+									<IconButton
+										aria-label="add interest"
+										onClick={addInterest}
+
+									>
+										{<AddIcon />}
+									</IconButton>
+								</InputAdornment>
+							}
+						/>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} sm={7}>
+					<Paper component="ul" className={classes.root} elevation={0}>
+						{chipData.map((data) => {
+							return (
+								<li key={data} style={{ listStyle: "none" }}>
+									<Chip
+										label={data}
+										variant="outlined"
+										color="primary"
+										onDelete={handleDelete(data)}
+										className={classes.chip}
+									/>
+								</li>
+							)
+						})}
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<FormControlLabel
+						control={
+							<Checkbox
+								color="primary"
+								name="termsOfService"
+								checked={userContext.user.acceptedTerms}
+								onChange={setAcceptedTerms}
+							/>
+						}
+						label="I accept the Terms of Service"
+					/>
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	)
 }
--- src/views/SignIn/SignInView.tsx
+++ src/views/SignIn/SignInView.tsx
@@ -1,186 +1,186 @@
-import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
-import validator from "validator";
+import React, { useContext, useState } from "react"
+import { useHistory } from "react-router-dom"
+import validator from "validator"
 // MaterialUI
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import TextField from "@material-ui/core/TextField"
+import Link from "@material-ui/core/Link"
+import Paper from "@material-ui/core/Paper"
+import Box from "@material-ui/core/Box"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
 // Componenets
-import { Copyright } from "../../components/Copyright";
+import { Copyright } from "../../components/Copyright"
 // Images
-import Logo from "../../images/FlatMatch.png";
+import Logo from "../../images/FlatMatch.png"
 // Styles
-import { SignInStyles } from "./SignInView.style";
+import { SignInStyles } from "./SignInView.style"
 // Context
-import { UserContext } from "../../App";
-import { IUser } from "../../models/user";
+import { UserContext } from "../../App"
+import { IUser } from "../../models/user"
 
 export default function SignInSide() {
-  const userContext = useContext(UserContext);
-  const classes = SignInStyles();
-  const [signUp, setSignUp] = useState(false);
-  const [passwordAgain, setPasswordAgain] = useState<string>("");
-  const history = useHistory();
+	const userContext = useContext(UserContext)
+	const classes = SignInStyles()
+	const [signUp, setSignUp] = useState(false)
+	const [passwordAgain, setPasswordAgain] = useState<string>("")
+	const history = useHistory()
 
-  const handleSubmit = () => {
-    history.push("home/create_profile");
-  }
+	const handleSubmit = () => {
+		history.push("home/create_profile")
+	}
 
-  const isDisabled = (): boolean => {
-	  return signUp
-	  ? !isSignUpValid(
-		  userContext.user.email,
-		  userContext.user.password,
-		  passwordAgain
-		)
-	  : !isSignInValid(
-		  userContext.user.email,
-		  userContext.user.password
-		)
-  }
+	const isDisabled = (): boolean => {
+		return signUp
+			? !isSignUpValid(
+				userContext.user.email,
+				userContext.user.password,
+				passwordAgain
+			)
+			: !isSignInValid(
+				userContext.user.email,
+				userContext.user.password
+			)
+	}
 
-  const setEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser: IUser = {
-      ...userContext.user,
-      email: event.target.value,
-    };
-    userContext.setUser(newUser);
-  };
+	const setEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newUser: IUser = {
+			...userContext.user,
+			email: event.target.value,
+		}
+		userContext.setUser(newUser)
+	}
 
-  const setPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newUser: IUser = {
-      ...userContext.user,
-      password: event.target.value,
-    };
-    userContext.setUser(newUser);
-  };
+	const setPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newUser: IUser = {
+			...userContext.user,
+			password: event.target.value,
+		}
+		userContext.setUser(newUser)
+	}
 
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <img src={Logo} className={classes.logo} alt="" />
-          <Grid container>
-            <Grid item xs>
-              <Typography component="h1" align="center" variant="h5">
-                Welcome,
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid item xs>
-              <Typography component="h2" align="center" variant="subtitle1">
-                {signUp ? "Sign up to get started!" : "Sign in to continue!"}
-              </Typography>
-            </Grid>
-          </Grid>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={userContext.user.email}
-              onChange={setEmail}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password (min. 8 characters)"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={userContext.user.password}
-              onChange={setPassword}
-            />
-            {signUp ? (
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password again"
-                label="Password again"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={passwordAgain}
-                onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
-                  setPasswordAgain(ev.target.value)
-                }
-              />
-            ) : null}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              disabled={isDisabled()}
-              onClick={handleSubmit}
-            >
-              {signUp ? "Sign Up" : "Sign In"}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link
-                  href="#"
-                  variant="body2"
-                  align="right"
-                  onClick={() => {
-                    setSignUp(!signUp);
-                  }}
-                >
-                  {signUp
-                    ? "Already a member? Sign In Here"
-                    : "New user? Sign Up Here"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
+	return (
+		<Grid container component="main" className={classes.root}>
+			<CssBaseline />
+			<Grid item xs={false} sm={4} md={7} className={classes.image} />
+			<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+				<div className={classes.paper}>
+					<img src={Logo} className={classes.logo} alt="" />
+					<Grid container>
+						<Grid item xs>
+							<Typography component="h1" align="center" variant="h5">
+								Welcome,
+							</Typography>
+						</Grid>
+					</Grid>
+					<Grid container>
+						<Grid item xs>
+							<Typography component="h2" align="center" variant="subtitle1">
+								{signUp ? "Sign up to get started!" : "Sign in to continue!"}
+							</Typography>
+						</Grid>
+					</Grid>
+					<form className={classes.form} noValidate>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+							value={userContext.user.email}
+							onChange={setEmail}
+						/>
+						<TextField
+							variant="outlined"
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password (min. 8 characters)"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+							value={userContext.user.password}
+							onChange={setPassword}
+						/>
+						{signUp ? (
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								name="password again"
+								label="Password again"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+								value={passwordAgain}
+								onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+									setPasswordAgain(ev.target.value)
+								}
+							/>
+						) : null}
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							color="primary"
+							className={classes.submit}
+							disabled={isDisabled()}
+							onClick={handleSubmit}
+						>
+							{signUp ? "Sign Up" : "Sign In"}
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link
+									href="#"
+									variant="body2"
+									align="right"
+									onClick={() => {
+										setSignUp(!signUp)
+									}}
+								>
+									{signUp
+										? "Already a member? Sign In Here"
+										: "New user? Sign Up Here"}
+								</Link>
+							</Grid>
+						</Grid>
+						<Box mt={5}>
+							<Copyright />
+						</Box>
+					</form>
+				</div>
+			</Grid>
+		</Grid>
+	)
 }
 
 function isSignInValid(email: string, password: string): boolean {
-  return isEmailValid(email) && password.length >= 8;
+	return isEmailValid(email) && password.length >= 8
 }
 
 function isSignUpValid(
-  email: string,
-  password: string,
-  passwordAgain: string
+	email: string,
+	password: string,
+	passwordAgain: string
 ): boolean {
-  return (
-    isEmailValid(email) && password.length >= 8 && password === passwordAgain
-  );
+	return (
+		isEmailValid(email) && password.length >= 8 && password === passwordAgain
+	)
 }
 
 function isEmailValid(email: string): boolean {
-  return validator.isEmail(email);
+	return validator.isEmail(email)
 }
