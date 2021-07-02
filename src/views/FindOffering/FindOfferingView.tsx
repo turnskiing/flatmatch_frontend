bc5296ea3cd38401c168a2bd28d8f42f1b689fc8
Tom Wenzel <tom.wenzel@limehome.de>
2021-07-02 17:37:26 +0200

    added breadcrumb to findOffering Page

--- src/views/FindOffering/FindOfferingView.tsx
+++ src/views/FindOffering/FindOfferingView.tsx
@@ -2,15 +2,47 @@ import TinderCards from "./TinderCards"
 import SwipeButtons from './SwipeButtons'
 import Header from "../../components/Header"
 
+
+import React, { useContext } from "react"
+import Paper from "@material-ui/core/Paper"
+import Stepper from "@material-ui/core/Stepper"
+import Step from "@material-ui/core/Step"
+import StepLabel from "@material-ui/core/StepLabel"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+import StepConnector from '@material-ui/core/StepConnector'
+
+// Context
+import { UserContext } from "../../App"
+import { useHistory } from "react-router-dom"
+import UserService from "../../services/UserService"
+import { AuthRoutes, NonAuthRoutes } from "../../Router"
+//Styles
+import grey from "@material-ui/core/colors/grey"
+
+
+import { FindOfferingBreadCrumb } from "../../components/Breadcrumbs"
+import DefaultAppBar from "../../components/DefaultAppBar"
+
+
 function FindOffering() {
+	const userContext = useContext(UserContext)
+
+
+
 	return (
-		<div className="app">
-			<Header title={""} />
-			<TinderCards />
-			<SwipeButtons />
-			{/*Tinder Cards*/}
-			{/*Buttons*/}
-		</div>
+		<React.Fragment>
+			{DefaultAppBar(userContext.user.first_name, FindOfferingBreadCrumb(), "")}
+
+			<div className="app">
+				<Header title={""} />
+				<TinderCards />
+				<SwipeButtons />
+				{/*Tinder Cards*/}
+				{/*Buttons*/}
+			</div>
+		</React.Fragment>
+
 	)
 }
 
