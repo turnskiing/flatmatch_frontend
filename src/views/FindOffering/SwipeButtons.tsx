8af87047d3a70171c458a8815538063330016a3c
Tom Wenzel <tom.wenzel@limehome.de>
2021-07-20 18:19:08 +0200

    added filter functionality to findOfferingView

--- src/views/FindOffering/SwipeButtons.tsx
+++ src/views/FindOffering/SwipeButtons.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./SwipeButtons.css"
 import ReplayIcon from "@material-ui/icons/Replay"
 import CloseIcon from "@material-ui/icons/Close"
@@ -6,8 +6,21 @@ import StarRateIcon from "@material-ui/icons/StarRate"
 import FavoriteIcon from "@material-ui/icons/Favorite"
 import FlashOnIcon from "@material-ui/icons/FlashOn"
 import IconButton from "@material-ui/core/IconButton"
+import { IFilter } from "../../models/filter"
+import { FilterContext, UserContext } from "../../App"
+import { useHistory } from "react-router-dom"
+import FilterView from "../Filter/FilterView";
+
 
 function SwipeButtons() {
+	const filterContext = useContext(FilterContext)
+	const handlePersonalPreferences = () => {
+		const newFilter: IFilter = {
+			...filterContext.filter,
+			isShown: true,
+		}
+		filterContext.setFilter(newFilter)
+	}
 	return (
 		<div className={"swipeButtons"}>
 			<div className="swipeButtons">
@@ -27,10 +40,11 @@ function SwipeButtons() {
 					<FavoriteIcon fontSize="large" />
 				</IconButton>
 
-				{/*<IconButton className="swipeButtons__lightning">*/}
-				{/*	<FlashOnIcon fontSize="large" />*/}
-				{/*</IconButton>*/}
-			</div>
+				<IconButton className="swipeButtons__lightning" onClick={handlePersonalPreferences}>
+					<FlashOnIcon fontSize="large" />
+				</IconButton>
+                < FilterView />
+            </div>
 		</div>
 	)
 }
